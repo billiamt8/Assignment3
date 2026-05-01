5dd58f5303c4f98477f6fd395d2dfe0f6df2766e
Billiamt8 <0408493@schoolsnet.act.edu.au>
2026-05-01 16:05:52 +1000

    Added highlighting of buttons and ability to navigate pages

--- Form1.cs
+++ Form1.cs
@@ -5,32 +5,19 @@ namespace Assignment3
         public MenuPage()
         {
             InitializeComponent();
-        }
-
-        private void btnHome_Click(object sender, EventArgs e)
-        {
+            SetupLayout();
             ShowScreen(new UC_Home());
         }
 
-        private void btnInventory_Click(object sender, EventArgs e)
+        private void SetupLayout()
         {
-            ShowScreen(new UC_Inventory());
+            btnHome.Click += (s, e) => ShowScreen(new UC_Home());
+            btnInventory.Click += (s, e) => ShowScreen(new UC_Inventory());
+            btnProducts.Click += (s, e) => ShowScreen(new UC_Products());
+            btnBuildOrders.Click += (s, e) => ShowScreen(new UC_BuildOrders());
+            btnCheckout.Click += (s, e) => ShowScreen(new UC_Checkout());
         }
-
-        private void btnProducts_Click(object sender, EventArgs e)
-        {
-            ShowScreen(new UC_Products());
-        }
-
-        private void btnBuildOrders_Click(object sender, EventArgs e)
-        {
-            ShowScreen(new UC_BuildOrders());
-        }
-
-        private void btnCheckout_Click(object sender, EventArgs e)
-        {
-            ShowScreen(new UC_Checkout());
-        }
+ 
 
         private void ShowScreen(UserControl newScreen)
         {
@@ -42,6 +29,28 @@ namespace Assignment3
             pnlContent.Controls.Clear(); // Clear the controls
             newScreen.Dock = DockStyle.Fill; // Set Dock to Fill
             pnlContent.Controls.Add(newScreen); // Add the UC to Pnl
+
+            if (newScreen is UC_Home)
+            {
+                HighlightButton(btnHome);
+            }
+            else if (newScreen is UC_Inventory)
+            {
+                HighlightButton(btnInventory);
+            }
+            else if (newScreen is UC_BuildOrders)
+            {
+                HighlightButton(btnBuildOrders);
+            }
+            else if (newScreen is UC_Checkout)
+            {
+                HighlightButton(btnCheckout);
+            }
+            else if (newScreen is UC_Products)
+            {
+                HighlightButton(btnProducts);
+            }
+
         }
 
         private void HighlightButton(Button buttonToHighlight)
